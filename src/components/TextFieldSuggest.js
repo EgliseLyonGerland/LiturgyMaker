07ea580cc35084ca23dd13e5706e7ea33b1e7b97
Nicolas Bazille <oltodo@msn.com>
2019-09-03 16:30:40 +0200

    Remove unused line

--- src/components/TextFieldSuggest.js
+++ src/components/TextFieldSuggest.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import deburr from "lodash/deburr";
 import Autosuggest from "react-autosuggest";
 import match from "autosuggest-highlight/match";
 import parse from "autosuggest-highlight/parse";
