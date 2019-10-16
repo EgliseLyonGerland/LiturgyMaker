e94271a1d6c626180a7ac88df85abb5803a85407
Nicolas Bazille <oltodo@msn.com>
2019-10-16 07:04:34 +0200

    Use Adobe Hebrew as serif font

--- src/config/preview.js
+++ src/config/preview.js
@@ -2,89 +2,89 @@ export const documentWidth = 1920;
 export const documentHeight = 1080;
 
 export const fontFamilies = {
-  sourceSansProp: 'Source Sans Pro',
-  crimsonText: 'Crimson Text',
+  sansSerif: 'Source Sans Pro',
+  serif: 'Adobe Hebrew',
 };
 
 export const typography = {
   title: {
-    fontFamily: fontFamilies.sourceSansProp,
+    fontFamily: fontFamilies.sansSerif,
     fontSize: 90,
     fontWeight: 900,
   },
   announcementTitle: {
-    fontFamily: fontFamilies.sourceSansProp,
+    fontFamily: fontFamilies.sansSerif,
     fontSize: 90,
     fontWeight: 900,
   },
   announcementPage: {
-    fontFamily: fontFamilies.sourceSansProp,
+    fontFamily: fontFamilies.sansSerif,
     fontSize: 50,
     fontWeight: 600,
   },
   announcementItemTitle: {
-    fontFamily: fontFamilies.sourceSansProp,
+    fontFamily: fontFamilies.sansSerif,
     fontSize: 50,
     fontWeight: 700,
   },
   announcementItemDetail: {
-    fontFamily: fontFamilies.sourceSansProp,
+    fontFamily: fontFamilies.sansSerif,
     fontSize: 40,
     fontWeight: 600,
     opacity: 70,
   },
   verseTitle: {
-    fontFamily: fontFamilies.sourceSansProp,
+    fontFamily: fontFamilies.sansSerif,
     fontSize: 90,
     fontWeight: 900,
   },
   verseSubtitle: {
-    fontFamily: fontFamilies.sourceSansProp,
+    fontFamily: fontFamilies.sansSerif,
     fontSize: 90,
     fontWeight: 400,
     opacity: 80,
   },
   verseExcerpt: {
-    fontFamily: fontFamilies.crimsonText,
+    fontFamily: fontFamilies.serif,
     fontStyle: 'italic',
-    fontSize: 100,
+    fontSize: 90,
   },
   songTitle: {
-    fontFamily: fontFamilies.crimsonText,
+    fontFamily: fontFamilies.serif,
     fontStyle: 'italic',
     fontSize: 90,
   },
   chapterTitle: {
-    fontFamily: fontFamilies.sourceSansProp,
+    fontFamily: fontFamilies.sansSerif,
     fontSize: 90,
     fontWeight: 900,
   },
   sermonTitle: {
-    fontFamily: fontFamilies.crimsonText,
+    fontFamily: fontFamilies.serif,
     fontStyle: 'italic',
     fontSize: 80,
   },
   sermonBibleRef: {
-    fontFamily: fontFamilies.sourceSansProp,
+    fontFamily: fontFamilies.sansSerif,
     fontSize: 60,
     fontWeight: 700,
     opacity: 70,
   },
   sermonAuthor: {
-    fontFamily: fontFamilies.sourceSansProp,
+    fontFamily: fontFamilies.sansSerif,
     fontSize: 40,
     fontWeight: 600,
     opacity: 50,
   },
   sermonPlanNumber: {
-    fontFamily: fontFamilies.sourceSansProp,
+    fontFamily: fontFamilies.sansSerif,
     fontSize: 220,
     fontWeight: 900,
     fontStyle: 'italic',
     opacity: 20,
   },
   sermonPlanTitle: {
-    fontFamily: fontFamilies.crimsonText,
+    fontFamily: fontFamilies.serif,
     fontStyle: 'italic',
     fontSize: 50,
     opacity: 80,
