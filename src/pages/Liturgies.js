889d3cc2dee716525ccc6c3ed46a91b71b851cc7
Nicolas Bazille <oltodo@msn.com>
2021-02-03 20:00:25 +0100

    chore(liturgies): migrate save button to SaveButton component

--- src/pages/Liturgies.js
+++ src/pages/Liturgies.js
@@ -2,14 +2,10 @@ import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Zoom from '@material-ui/core/Zoom';
 import Container from '@material-ui/core/Container';
-import Fab from '@material-ui/core/Fab';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
-import SaveIcon from '@material-ui/icons/Save';
-import CheckIcon from '@material-ui/icons/Check';
 import CodeIcon from '@material-ui/icons/Code';
 import CloseIcon from '@material-ui/icons/Close';
 import BeatLoader from 'react-spinners/BeatLoader';
@@ -18,9 +14,9 @@ import locale from 'date-fns/locale/fr';
 import capitalize from 'lodash/capitalize';
 import debounce from 'lodash/debounce';
 import cloneDeep from 'lodash/cloneDeep';
-import classnames from 'classnames';
 import Form from '../components/Form';
 import Code from '../components/Code';
+import SaveButton from '../components/SaveButton';
 // import Preview from '../components/Preview';
 import generateCode from '../utils/generateCode';
 import {
@@ -38,28 +34,22 @@ import {
   selectAllRecitations,
 } from '../redux/slices/recitations';
 
-const gutters = 3;
+// const gutters = 3;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     marginBottom: '50vh',
   },
-  preview: {
-    margin: theme.spacing(gutters * 2, gutters),
-    maxWidth: 600,
-    flexGrow: 1,
+  // preview: {
+  //   margin: theme.spacing(gutters * 2, gutters),
+  //   maxWidth: 600,
+  //   flexGrow: 1,
 
-    '& > *': {
-      position: 'sticky',
-      top: theme.spacing(gutters * 2),
-    },
-  },
-  content: {
-    display: 'grid',
-    minHeight: '80vh',
-    gridTemplateRows: 'auto 1fr',
-    // marginTop: -theme.spacing(8),
-  },
+  //   '& > *': {
+  //     position: 'sticky',
+  //     top: theme.spacing(gutters * 2),
+  //   },
+  // },
   navBar: {
     display: 'flex',
     height: theme.spacing(8),
@@ -85,24 +75,6 @@ const useStyles = makeStyles((theme) => ({
   actions: {
     width: 50,
   },
-  cta: {
-    position: 'fixed',
-    bottom: theme.spacing(4),
-    left: '50%',
-    width: 200,
-    marginLeft: -200 / 2,
-    transition: 'background .3s',
-  },
-  ctaDisabled: {
-    background: `${theme.palette.secondary.main} !important`,
-    color: 'white !important',
-  },
-  ctaSaved: {
-    background: `#17d86d !important`,
-  },
-  ctaIcon: {
-    marginRight: theme.spacing(1),
-  },
   spinner: {
     display: 'flex',
     height: 200,
@@ -110,9 +82,9 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
   },
   [theme.breakpoints.down('md')]: {
-    preview: {
-      display: 'none',
-    },
+    // preview: {
+    //   display: 'none',
+    // },
     navBar: {
       padding: 0,
 
@@ -145,11 +117,11 @@ const formatDate = (date) => {
 const Liturgies = () => {
   const classes = useStyles();
   const [currentDate, setCurrentDate] = useState(getNextSundayDate(new Date()));
-  const [saved, setSaved] = useState(false);
   const [displayCode, setDisplayCode] = useState(false);
   const [focusedBlock, setFocusedBlock] = useState([-1]);
   const [persisting, setPersisting] = useState(false);
   const [persisted, setPersisted] = useState(true);
+  const [dirty, setDirty] = useState(false);
   const dispatch = useDispatch();
   const liturgyState = useSelector((state) =>
     selectLiturgyById(state, format(currentDate, 'yMMdd')),
@@ -171,6 +143,10 @@ const Liturgies = () => {
   );
 
   useEffect(() => {
+    setDirty(false);
+  }, [currentDate]);
+
+  useEffect(() => {
     if (songsStatus === 'idle') {
       dispatch(fetchSongs());
     }
@@ -183,7 +159,7 @@ const Liturgies = () => {
   }, [currentDate, dispatch, liturgy, recitationsStatus, songsStatus]);
 
   const handleBlocksChange = (blocks) => {
-    setPersisted(false);
+    setDirty(true);
     dispatch(setLiturgy({ ...liturgy, blocks }));
   };
 
@@ -202,88 +178,25 @@ const Liturgies = () => {
 
   const handleSave = async () => {
     setPersisting(true);
-
     await dispatch(persistLiturgy(liturgy));
-
     setPersisting(false);
     setPersisted(true);
-    setSaved(true);
-    setTimeout(() => {
-      setSaved(false);
-    }, 2000);
+    setDirty(false);
   };
 
   const handleAddBlock = (index, data) => {
-    setPersisted(false);
+    setDirty(true);
     dispatch(addLiturgyBlock({ id: liturgy.id, index, data }));
   };
 
   const handleRemoveBlock = (index) => {
-    setPersisted(false);
+    setDirty(true);
     dispatch(removeLiturgyBlock({ id: liturgy.id, index }));
   };
 
   const handleFillFromLastWeek = (block) => {
-    setPersisted(false);
+    setDirty(true);
     dispatch(fillBlockFromPreviousWeek({ id: liturgy.id, block }));
-  };
-
-  let zoomKey = 'nothing';
-  if (saved) zoomKey = 'saved';
-  else if (!persisted) zoomKey = 'save';
-
-  const renderSaveButton = () => {
-    if (saved) {
-      return (
-        <Fab
-          variant="extended"
-          className={classnames(classes.cta, {
-            [classes.ctaSaved]: saved,
-          })}
-          color="secondary"
-          disabled
-          classes={{ disabled: classes.ctaDisabled }}
-        >
-          <CheckIcon className={classes.ctaIcon} />
-          Enregistré
-        </Fab>
-      );
-    }
-
-    if (persisting) {
-      return (
-        <Fab
-          variant="extended"
-          className={classnames(classes.cta, {
-            [classes.ctaSaved]: saved,
-          })}
-          color="secondary"
-          disabled
-          classes={{ disabled: classes.ctaDisabled }}
-        >
-          <BeatLoader key="saving" color="white" size={8} sizeUnit="px" />
-        </Fab>
-      );
-    }
-
-    if (!persisted) {
-      return (
-        <Fab
-          aria-label="Sauvegarder"
-          variant="extended"
-          className={classnames(classes.cta, {
-            [classes.ctaSaved]: saved,
-          })}
-          color="secondary"
-          onClick={() => handleSave()}
-        >
-          <SaveIcon className={classes.ctaIcon} />
-          Enregistrer
-        </Fab>
-      );
-    }
-
-    return <div />;
   };
 
   const renderNavBar = () => (
@@ -368,9 +281,14 @@ const Liturgies = () => {
           />
         </div> */}
       </Container>
-      <Zoom key={zoomKey} in={true}>
-        {renderSaveButton()}
-      </Zoom>
+
+      <SaveButton
+        status={
+          persisting ? 'running' : persisted ? 'done' : dirty ? 'ready' : null
+        }
+        onClick={handleSave}
+        onHide={() => setPersisted(false)}
+      />
     </div>
   );
 };
