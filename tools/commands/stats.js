c39278f345c6401b1a308b00a3114c4a1d1daa1c
Nicolas Bazille <oltodo@msn.com>
2021-09-29 22:50:12 +0200

    feat(tools): generate songs stats by year

--- tools/commands/stats.js
+++ tools/commands/stats.js
@@ -5,25 +5,29 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 const open = require('open');
 const { format } = require('date-fns');
 const googleServiceCreds = require('../config/~egliselyongerland-642b3dfa5d11.json');
+const { table } = require('table');
+const { uniq, reduce, map, sum, tail, last } = require('lodash');
 
 module.exports.command = `stats <command>`;
 module.exports.desc = 'Display stats';
 
 const backupDir = `${__dirname}/../../.firebase/backup`;
 
-async function songsCommand({ update }) {
+async function songsCommand({ dryRun }) {
   let liturgies = JSON.parse(readFileSync(`${backupDir}/liturgies.json`));
   let songs = JSON.parse(readFileSync(`${backupDir}/songs.json`));
 
   const stats = songs.reduce(
     (acc, { id, title }) => ({
       ...acc,
-      [id]: { title, count: 0 },
+      [id]: { title, count: {} },
     }),
     {},
   );
 
   liturgies.forEach((liturgy) => {
+    const year = new Date(liturgy.date).getFullYear() + 0;
+
     liturgy.blocks.forEach((block) => {
       if (block.type !== 'songs') {
         return;
@@ -32,12 +36,33 @@ async function songsCommand({ update }) {
       const items = liturgy.version > 5 ? block.data.items : block.data;
 
       items.forEach(({ id }) => {
-        if (id) {
-          stats[id].count += 1;
+        if (!id) return;
+
+        if (!(year in stats[id].count)) {
+          stats[id].count[year] = 0;
         }
+
+        stats[id].count[year] += 1;
       });
     });
   });
+
+  const years = uniq(
+    reduce(stats, (acc, curr) => acc.concat(Object.keys(curr.count)), []),
+  );
+
+  const header = ['Titre', ...years, 'Total'];
+
+  const values = map(stats, (item) => [
+    item.title,
+    ...years.map((year) => item.count[year] || 0),
+    sum(Object.values(item.count)),
+  ]).sort((a, b) => last(b) - last(a));
+
+  if (dryRun) {
+    console.log(table([header, ...values]));
+    return;
+  }
 
   const doc = new GoogleSpreadsheet(
     '16KvjnOV9mygprQv1X47jrJ-jgfJQ9f2pXuA3CJA6Iig',
@@ -49,18 +74,13 @@ async function songsCommand({ update }) {
   const sheet = doc.sheetsByTitle.Chants;
 
   await sheet.clear();
-  await sheet.setHeaderRow(['Titre', 'Compteur']);
-  await sheet.addRows(
-    sortBy(stats, 'count')
-      .reverse()
-      .map((row) => ({ Titre: row.title, Compteur: row.count })),
-  );
-
+  await sheet.setHeaderRow(header);
+  await sheet.addRows(values);
   await sheet.loadCells('B1:B1');
 
   const firstDate = new Date(liturgies[0].date);
   const lastDate = new Date(liturgies[liturgies.length - 1].date);
-  const b1 = sheet.getCell(0, 1);
+  const b1 = sheet.getCell(0, 0);
   b1.note = `Calculé sur les ${liturgies.length} cultes du ${format(
     firstDate,
     'dd/MM/yyyy',
@@ -74,5 +94,12 @@ async function songsCommand({ update }) {
 }
 
 module.exports.builder = function builder(yargs) {
-  yargs.command('songs', 'Display stats about songs', noop, songsCommand);
+  yargs.command(
+    'songs',
+    'Display stats about songs',
+    (yargs) => {
+      yargs.option('dry-run', { type: 'boolean', default: false });
+    },
+    songsCommand,
+  );
 };
