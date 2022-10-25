2ca381ea302d05d593b0b79c61a31e56066b99cb
Nicolas Bazille <oltodo@msn.com>
2022-10-25 23:37:18 +0200

    feat(code): update format according to the slide generator tool

--- src/utils/generateCode.ts
+++ src/utils/generateCode.ts
@@ -1,11 +1,20 @@
-import { cloneDeep } from 'lodash';
 import find from 'lodash/find';
 
 import type {
   LiturgyDocument,
+  LiturgyBlock,
   RecitationDocument,
   SongDocument,
+  BlockType,
 } from '../types';
+
+const isEmpty: {
+  [K in BlockType]?: (block: LiturgyBlock<K>) => boolean;
+} = {
+  openDoors: (block) => {
+    return block.data.title.trim() === '';
+  },
+};
 
 export default function generateCode(
   doc: LiturgyDocument,
@@ -14,27 +23,43 @@ export default function generateCode(
     recitations,
   }: { songs: SongDocument[]; recitations: RecitationDocument[] },
 ) {
-  const result = doc.blocks.map((block) => {
-    const data: any = cloneDeep(block.data);
+  const result = doc.blocks.reduce<{ type: string; data: any }[]>(
+    (acc, block) => {
+      if (block.type === 'openDoors' && isEmpty.openDoors?.(block)) {
+        return acc;
+      }
 
-    if (block.type === 'songs') {
-      const items = block.data.items.map((item) => {
-        const song = find(songs, ['id', item.id]);
+      if (block.type === 'songs') {
+        return acc.concat(
+          block.data.items.map((data) => ({
+            type: 'song',
+            data: { ...data, ...find(songs, ['id', data.id]) },
+          })),
+        );
+      }
 
-        return { ...item, song };
-      });
+      if (block.type === 'recitation') {
+        return acc.concat([
+          {
+            type: 'recitation',
+            data: find(recitations, ['id', block.data.id]),
+          },
+        ]);
+      }
 
-      data.items = items;
-    }
+      if (block.type === 'reading') {
+        return acc.concat(
+          block.data.bibleRefs.map((data) => ({
+            type: 'verse',
+            data,
+          })),
+        );
+      }
 
-    if (block.type === 'recitation') {
-      const recitation = find(recitations, ['id', block.data.id]);
-
-      data.recitation = recitation;
-    }
-
-    return { type: block.type, data };
-  });
+      return acc.concat([{ type: block.type, data: block.data }]);
+    },
+    [],
+  );
 
   return JSON.stringify(result, null, '  ');
 }
